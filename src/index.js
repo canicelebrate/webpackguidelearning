ea6c485d938f69872abee9e7ea2b4c3a932c2f30
william_wang <william_wang@trimble.com>
2017-12-31 22:18:34 +0800

    Lazy Loading Final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import _ from 'lodash';
 
  if (process.env.NODE_ENV !== 'production') {
    console.log('Looks like we are in development mode!');
@@ -7,19 +8,35 @@ import './style.css';
    console.log('Looks like we are in production mode!');
  }
 
- function getComponent() {
-   return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+ function component() {
+
      var element = document.createElement('div');
 
-     element.innerHTML = _.join(['Hello', 'webpack','code','splitting','dynamic','imports'], ' ');
+     element.innerHTML = _.join(['Hello', 'webpack','lazy','loading'], ' ');
+
+	 
+	 var button = document.createElement('button');
+     var br = document.createElement('br');
+
+     button.innerHTML = 'Click me and look at the console!';
+     element.appendChild(br);
+     element.appendChild(button);
+
+   
+     // Note that because a network request is involved, some indication
+     // of loading would need to be shown in a production-level site/app.
+     button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+     var print = module.default;
+
+     print();
+   });
+
 
      return element;
 
-   }).catch(error => 'An error occurred while loading the component');
  }
- 
- getComponent().then(component=>{
-	 document.body.appendChild(component);
- });
+
+document.body.appendChild(component());
+
  
 
