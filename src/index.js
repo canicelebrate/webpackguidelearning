b3e595b900df4ea6a7f3ad389bd5a65cff7ac1c7
william_wang <william_wang@trimble.com>
2018-01-02 22:25:54 +0800

    Authoring a Library Final

--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
-import './style.css';
 import _ from 'lodash';
+import numRef from './ref.json';
 
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
+};
 
- if (process.env.NODE_ENV !== 'production') {
-   console.log('Looks like we are in development mode!');
- }
- else{
-   console.log('Looks like we are in production mode!');
- }
-
- function component() {
-
-     var element = document.createElement('div');
-
-     element.innerHTML = _.join(['Hello', 'webpack','lazy','loading'], ' ');
-
-	 
-	 var button = document.createElement('button');
-     var br = document.createElement('br');
-
-     button.innerHTML = 'Click me and look at the console2!';
-     element.appendChild(br);
-     element.appendChild(button);
-
-   
-     // Note that because a network request is involved, some indication
-     // of loading would need to be shown in a production-level site/app.
-	button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-	 var print = module.default;
-
-	 print();
-	});
-
-
-     return element;
-
- }
-
-document.body.appendChild(component());
-
- 
-
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+};
